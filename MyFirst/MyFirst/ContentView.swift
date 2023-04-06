36f41deb79249fdc3927d8efaa36142314e36300
kuehar <green95251@gmail.com>
2023-04-06 20:50:23 +0900

    first commit

---
+++ MyFirst/MyFirst/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MyFirst
+//
+//  Created by kuehar on 2023/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Swift!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ MyFirst/MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by kuehar on 2023/04/04.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
