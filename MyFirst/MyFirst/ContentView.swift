5660cd65cd7406af60c3a3ce16166c03defefbf4
kuehar <green95251@gmail.com>
2023-04-06 21:04:45 +0900

    Day1 2-6まで終了

--- MyFirst/MyFirst/ContentView.swift
+++ MyFirst/MyFirst/ContentView.swift
@@ -8,9 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var outputText = "Hello Swift!!"
+    
     var body: some View {
-        Text("Hello, Swift!")
+        VStack{
+        Text(outputText)
+            .font(.largeTitle)
+            .fontWeight(.light)
+            .foregroundColor(Color.black)
             .padding()
+            
+            Button(action:{
+                outputText = "Hi,SwiftUI!!"
+            }){
+            Text("切り替えボタン")
+            .padding(.all)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+            .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+            }
+        }
     }
 }
 
